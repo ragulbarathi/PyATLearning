ff8e3899c03f1acd9294d4b5c451dbae7c5652fd
ragulbarathi <ragulbarathi98@gmail.com>
2024-08-10 09:52:52 +0530

    Lab003 added builtin about print function

---
+++ src/ex_10082021/Lab003.py
@@ -0,0 +1,17 @@
+# Python Program
+print("Hello World!")
+
+# Builtin.py all the important already built by Python guys
+# these are like utilities.
+
+# print() -
+# self - Concept in OOps which points to itself - ignore.
+# *args - Unlimited number of arguments * - string, int, float, boolean..
+# file=None - File IO
+
+
+# sep=' ' - How you want to separate the arguments
+# end='\n' - in the end what you want to do
+
+print("Hello", "World", "Amit", "Ragul", 123, 3.14, True, sep="|", end="  ")
+print("Ragul")
