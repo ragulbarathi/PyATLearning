97ec35eb65e944c7a35ee9e408cfb4754534ff35
ragulbarathi <ragulbarathi98@gmail.com>
2024-08-10 09:56:55 +0530

    About source code,python rules

---
+++ src/ex_10082021/Lab005.py
@@ -0,0 +1,6 @@
+# 1+1=2 #SyntaxError
+# Source Code - Human readable code,code written by the humans - HLL
+# Source Code -> We have to follow some rules - Python rules
+
+print("Good", "Ragul", sep="?", end="_")
+print("Amit")
