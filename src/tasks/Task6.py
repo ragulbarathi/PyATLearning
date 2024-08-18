716a30df7e50f00dd73b69279118ae0a6f2d47b8
ragulbarathi <ragulbarathi98@gmail.com>
2024-08-18 09:27:29 +0530

    Task 6 added

---
+++ src/tasks/Task6.py
@@ -0,0 +1,4 @@
+"""- Develop a Python script that calculates the square and cube of a given number"""
+num = float(input('Enter a number: -'))
+print(f"(number)^2={num ** 2}")
+print(f"(number)^3={pow(num, 3)}")
