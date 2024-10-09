f1cca435f30b903f36860633e228a378fa47b087
ragulbarathi <ragulbarathi98@gmail.com>
2024-10-09 12:13:27 +0530

    Added Request module with pytest and allure report

--- src/Oct/test_RestfullBookerCollections.py
+++ src/Oct/test_RestfullBookerCollections.py
@@ -1,2 +1,25 @@
-def test_TC1():
-    print("TC1")
+import pytest
+import allure
+
+
+@pytest.mark.smoke
+@allure.title("Smoke Test report")
+@allure.description("Smoke Test allure report")
+@allure.tag("smoke")
+@allure.severity(allure.severity_level.CRITICAL)
+@allure.label("owner", "Ragul")
+@allure.link("https://dev.example.com/", name="Website")
+@allure.issue("AUTH-123")
+@allure.testcase("TMS-456")
+def test_tc1():
+    assert 1 + 1 == 2
+
+
+@pytest.mark.smoke
+def test_tc2():
+    assert 1 + 1 == 3
+
+
+@pytest.mark.reg
+def test_tc3():
+    assert 1 - 1 == 0
